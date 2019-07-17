c620a6b3d59ffbee9d9f2734b72cb18de4fea6f1
Antim Prisăcaru <antimpris@gmail.com>
2019-07-17 15:52:08 +0300

    Functionalities up to chapter 5

---
+++ resources-master/online-shop/src/app/_guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    let url: string = state.url;
+ 
+    return true;
+  }
+  
+}
